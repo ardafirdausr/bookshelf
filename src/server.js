90df80eded50707c2b1e147b479235acf42e6c3a
Arda Firdaus Ramadhan <ardafirdausr@gmail.com>
2021-05-16 18:26:28 +0700

    add minimal hapi web server

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
