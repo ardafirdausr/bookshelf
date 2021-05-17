b51a684b328c445766168f9428673fc1200d52ad
Arda Firdaus Ramadhan <ardafirdausr@gmail.com>
2021-05-17 09:38:39 +0700

    add eslint

--- src/server.js
+++ src/server.js
@@ -1,29 +1,28 @@
+/* eslint-disable no-console */
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
+  const server = Hapi.server({
+    port: 8000,
+    host: 'localhost',
+  });
 
-    const server = Hapi.server({
-        port: 8000,
-        host: 'localhost'
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      const { name = 'World' } = request.query;
+      return h.response(`Hello ${name}`);
+    },
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-
-            return 'Hello World!';
-        }
-    });
-
-    await server.start();
-    console.log('Server running on %s', server.info.uri);
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
